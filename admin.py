bbd683824b43c180bfbb4e6598d2fb4ec008fcad
varikin <varikin@overdrive.local>
2008-11-22 16:35:15 -0600

    Added Fermentable and Yeast models.
    Only seven models to go!

--- admin.py
+++ admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from django_beer.models import Hop
+from django_beer.models import Fermentable, Hop, Yeast
 
+class FermentableAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    
 class HopAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
+class YeastAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(Hop, HopAdmin)
+admin.site.register(Fermentable, FermentableAdmin)
+admin.site.register(Yeast, YeastAdmin)
--- models.py
+++ models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
 class Hop(models.Model):
-    """
-    Represents hop as defined by beerxml.  
-    
-    See http://beerxml.com/beerxml.htm for details.
-    """
+    """Represents hop as defined by beerxml."""
     USE_CHOICES = (
         ('boil', 'Boil'),
         ('dry_hop', 'Dry Hop'),
@@ -28,11 +24,11 @@ class Hop(models.Model):
     
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
-    version = models.SmallIntegerField(default=1)
+    version = models.PositiveSmallIntegerField(default=1)
     alpha = models.FloatField()
     amount = models.FloatField()
     use = models.CharField(max_length=10, choices=USE_CHOICES)
-    time = models.IntegerField()
+    time = models.FloatField()
     notes = models.TextField(blank=True, null=True)
     hop_type = models.CharField(blank=True, null=True, max_length=10, choices=TYPE_CHOICES)
     form = models.CharField(blank=True, null=True, max_length=10, choices=FORM_CHOICES)  
@@ -44,3 +40,87 @@ class Hop(models.Model):
     caryophyllene = models.FloatField(blank=True, null=True)
     cohumulone = models.FloatField(blank=True, null=True)
     myrcene = models.FloatField(blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.name
+    
+class Fermentable(models.Model):
+    """Represents fermentables as defined by beerxml."""
+    
+    TYPE_CHOICES = (
+        ('grain', 'Grain'),
+        ('sugar', 'Sugar'),
+        ('extract', 'Extract'),
+        ('dryextract', 'Dry Extract'),
+        ('adjunct', 'Adjunct'),
+    )
+    
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    version = models.PositiveSmallIntegerField(default=1)
+    ferm_type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    amount = models.FloatField()
+    ferm_yield = models.FloatField()
+    color = models.FloatField()
+    add_after_boil = models.BooleanField(default=False)
+    origin = models.CharField(blank=True, null=True, max_length=100)
+    supplier = models.CharField(blank=True, null=True, max_length=100)
+    notes = models.TextField(blank=True, null=True)
+    coarse_fine_diff = models.FloatField(blank=True, null=True)
+    moisture = models.FloatField(blank=True, null=True)
+    diastic_power = models.FloatField(blank=True, null=True)
+    protein = models.FloatField(blank=True, null=True)
+    max_in_batch = models.FloatField(blank=True, null=True),
+    recommend_mash = models.BooleanField(default=False)
+    ibu_gal_per_lb = models.FloatField(blank=True, null=True)
+
+    def __unicode__(self):
+         return self.name
+
+class Yeast(models.Model):
+    """Represents yeast as defined by beerxml."""
+    
+    TYPE_CHOICES = (
+        ('ale', 'Ale'),
+        ('lager', 'Lager'),
+        ('wheat', 'Wheat'),
+        ('wine', 'Wine'),
+        ('champagne', 'Champagne'),
+    )
+    
+    FORM_CHOICES = (
+        ('liquid', 'Liquid'),
+        ('dry', 'Dry'),
+        ('slant', 'Slant'),
+        ('culture', 'Culture'),
+    )
+    
+    FLOCCULATION_CHOICES = (
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+        ('very_high', 'Very High'),
+    )
+    
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    version = models.PositiveSmallIntegerField(default=1)
+    yeast_type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    form = models.CharField(max_length=10, choices=FORM_CHOICES)
+    amount = models.FloatField()
+    amount_is_weight = models.BooleanField(default=False)
+    laboratory = models.CharField(blank=True, null=True, max_length=100)
+    product_id = models.CharField(blank=True, null=True, max_length=100)
+    min_temperature = models.FloatField(blank=True, null=True)
+    max_temperature = models.FloatField(blank=True, null=True)
+    flocculation = models.CharField(blank=True, null=True, max_length=10, choices=FLOCCULATION_CHOICES)
+    attenuation = models.FloatField(blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    best_for = models.CharField(blank=True, null=True, max_length=200)
+    times_cultured = models.IntegerField(blank=True, null=True)
+    max_reuse = models.IntegerField(blank=True, null=True)
+    add_to_secondary = models.BooleanField(default=False)
+    
+    def __unicode__(self):
+        return self.name
+
